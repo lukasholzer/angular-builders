58d3328bda164684c4aef9fe70fcb9c31fb2e364
Lukas Holzer <lukas.holzer@dynatrace.com>
2019-12-15 17:07:27 +0100

    feat(graphql): Use gaphql service generation inside builder.

--- packages/graphql/src/build/build.ts
+++ packages/graphql/src/build/build.ts
@@ -5,11 +5,22 @@ import {
 } from '@angular-devkit/architect';
 import { BuilderOptions } from './builder-options';
 import { json } from '@angular-devkit/core';
+import { generateGraphQlServices } from './generate-graphql-services';
 
 export async function run(
   options: BuilderOptions,
   context: BuilderContext
 ): Promise<BuilderOutput> {
+
+  try {
+    await generateGraphQlServices(options)
+  } catch (error) {
+    context.logger.error(error);
+    return { success: false };
+  }
+
+  context.logger.info(`\n\n✔︎ Successfully generated graphql services!`);
+
   return {
     success: true
   };
