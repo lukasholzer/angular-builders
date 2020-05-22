664a5050a9ea077d43a4679263f3bf0a9eae4a2c
Lukas Holzer <lukas.holzer@dynatrace.com>
2020-05-22 22:31:35 +0200

    refactor: Refactor to nx workspace structure.
    Refactor from lerna to nx workspace structure as it has better
    tooling support for developing angular or nx plugins.

--- packages/graphql/src/builders/build/generate-graphql-services.ts
+++ packages/graphql/src/builders/build/generate-graphql-services.ts
@@ -1,7 +1,7 @@
 import { generate } from '@graphql-codegen/cli';
 import { writeFileSync } from 'fs';
-import { join, basename } from 'path';
-import { BuilderOptions } from './builder-options';
+import { basename, join } from 'path';
+import { BuildBuilderSchema } from './schema';
 
 interface GeneratedResult {
   filename: string;
@@ -9,22 +9,9 @@ interface GeneratedResult {
   hooks: object;
 }
 
-export async function generateGraphQlServices(
-  options: BuilderOptions
-): Promise<void> {
-  const files = await graphQlCodeGenerator(options);
-
-  const barrelFile = files
-    .map(file => `export * from './${basename(file.filename, '.ts')}';`)
-    .sort()
-    .join('\n');
-
-  await writeFileSync(join(options.outputPath, 'index.ts'), barrelFile);
-}
-
 /** Generates the apollo graphql services */
 async function graphQlCodeGenerator(
-  options: BuilderOptions
+  options: BuildBuilderSchema
 ): Promise<GeneratedResult[]> {
   const {
     schema,
@@ -44,29 +31,42 @@ async function graphQlCodeGenerator(
       generates: {
         [join(outputPath, declarationFile)]: {
           config,
-          plugins: ['typescript', 'fragment-matcher']
+          plugins: ['typescript', 'fragment-matcher'],
         },
         [join(outputPath)]: {
           config,
           preset: 'near-operation-file',
           presetConfig: {
             baseTypesPath: declarationFile,
-            folder: modelsFolder
+            folder: modelsFolder,
           },
           plugins: [
             'typescript-operations',
             'typescript-compatibility',
-            'typescript-apollo-angular'
-          ]
-        }
-      }
+            'typescript-apollo-angular',
+          ],
+        },
+      },
     });
   } catch (error) {
     if (Array.isArray(error.errors)) {
       console.error(error.errors);
       for (const problem of error.errors) {
-        throw problem;
+        throw new Error(problem);
       }
     }
   }
+}
+
+export async function generateGraphQlServices(
+  options: BuildBuilderSchema
+): Promise<void> {
+  const files = await graphQlCodeGenerator(options);
+
+  const barrelFile = files
+    .map((file) => `export * from './${basename(file.filename, '.ts')}';`)
+    .sort()
+    .join('\n');
+
+  await writeFileSync(join(options.outputPath, 'index.ts'), barrelFile);
 }
